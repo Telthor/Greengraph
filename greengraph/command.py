7fee7c276504d6855a2dc977ad3e5c355b1d54d3
David Wise <wisedavid@mac.com>
2016-01-08 13:09:12 +0000

    Created command line interface file that allows calling of greengraph function

--- greengraph/command.py
+++ greengraph/command.py
@@ -1,11 +1,23 @@
 from argparse import ArgumentParser
 from greengraph import Greengraph
+from matplotlib import pyplot as plt
 
 def process():
     parser = ArgumentParser(description = 'Calculate amount of greenspace between two points')
 
-    parser.add_argument('--from','-f')
+    parser.add_argument('--beginning','-b')
     parser.add_argument('--to','-t')
     parser.add_argument('--steps','-s')
     parser.add_argument('--out','-o')
+
+    arguments = parser.parse_args()
+
+    my_graph = Greengraph(arguments.beginning, arguments.to)
+    plt.plot(my_graph.green_between(arguments.steps))
+    plt.show()
+
+if __name__ == '__main__':
+    process()
+    
+    
 
