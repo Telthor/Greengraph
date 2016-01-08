966f3e80677099767f07a90f51b5aeb096369ad8
David Wise <wisedavid@mac.com>
2016-01-08 17:19:13 +0000

    legacy file tests committed

--- legacy/First_test.py
+++ legacy/First_test.py
@@ -1,6 +1,8 @@
 from greengraph import Greengraph
 from matplotlib import pyplot as plt
+from map import Map
 mygraph = Greengraph('London', 'Edinburgh')
 data = mygraph.green_between(200)
 plt.plot(data)
 plt.show()
+Map.show_green(mygraph)
